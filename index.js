862bf1e24eec4969a4fb3fad9ae61a56fbefbb62
turnerran <55016001+turnerran@users.noreply.github.com>
2022-06-01 22:24:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,13 @@ app.get("/api/courses", (req, res) => {});
 
 // implement this api to be able to recive an object containing a course name and add it to
 // the courses array
+
+// bonus points - implement this to be able to accept a query param (name) that will only return courses that start with
+// that name. i.e: http://localhost:3000/api/courses?name=cou -> return all courses
+// http://localhost:3000/api/courses?name=course 3 -> return just course 3
 app.post("/api/courses", (req, res) => {});
 
 // implement this api to retrive a course when accepting an id as a route param
-// bonus points - implement this to be able to accept a query param (filter) that will only return this
-// filter property value. i.e - /api/courses/2?filter=name should return {course 2}
 app.get("/api/courses/:id", (req, res) => {});
 
 const port = process.env.PORT || 3000;
